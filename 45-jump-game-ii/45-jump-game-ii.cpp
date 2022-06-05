dbc97160261cc856a4031815b96dbddf044c9e3a
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-06-05 14:56:33 +0530

    Time: 16 ms (86.96%), Space: 16.5 MB (96.66%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int jump(vector<int>& arr) {
+        int maxR = arr[0];
+        int step = arr[0];
+        int jump =1;
+        int n = arr.size();
+        if(n==1) return 0;
+        if(arr[0]==0) return -1;
+        
+        for(int i=1;i<n;i++){
+                
+            if(i==n-1) return jump;
+            maxR= max(maxR, arr[i]+i);
+            step--;
+            if(step==0){
+                jump++;
+                if(i>=maxR){
+                    return -1;
+                }
+                step = maxR-i;
+            }
+            
+            
+            
+        }
+        return jump;
+    }
+};
