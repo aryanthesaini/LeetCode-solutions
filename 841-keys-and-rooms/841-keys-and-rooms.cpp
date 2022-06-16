a96ff652e54aed0e6813c9dd4f70926773948673
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-06-16 17:35:28 +0530

    Time: 21 ms (19.59%), Space: 11.2 MB (10.97%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    
-    
-    void dfs(vector<int>&vis, int src, vector<int>g[]){
-        vis[src]=1;
-        for(auto x: g[src]){
-            if(!vis[x]){
-                dfs(vis, x, g);
-            }
-        }
-    }
-    
-    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        
-        vector<int>g[n];
+         vector<int>g[n];
         
         for(int i=0;i<n;i++){
             for(int j=0;j<rooms[i].size();j++){
                 g[i].push_back(rooms[i][j]);
             }
         }
-        
         vector<int>vis(n,0);
         
-        dfs(vis, 0, g);
+        queue<int>q;
+        q.push(0);
         
-        for(int i=0;i<n;i++){
-            cout<<i<<"->"<<vis[i]<<" ";
-            if(vis[i]==0) return false;
+        while(!q.empty()){
+            int temp = q.front();
+            vis[temp]=1;
+            for(auto x: g[temp]){
+              if(!vis[x]){
+                  q.push(x);
+              }
+            }
+            q.pop();
         }
-    
+        
+        for(auto x:vis){
+            if(x==0) return false;
+        }
         
         return true;
     }
