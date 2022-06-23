9155500cf789470317de799777dc5197d9844862
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-06-23 14:37:09 +0530

    Time: 2094 ms (5.00%), Space: 9 MB (93.82%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
-public:       
+public:
     int ans =0;
     
-    
-    void dfs(vector<int>& nums, int target, int curSum, int count){
-        if(count==nums.size()){
-            if(curSum==target) ans++;
+    void dfs(vector<int>& nums, int target, int curSum, int count, int n){
+        if(count == n){
+            if(curSum == target)ans++;
             return;
         }
-        dfs(nums,target, curSum+nums[count], count+1);
-        dfs(nums,target, curSum-nums[count], count+1);
+        
+        dfs(nums,target,curSum+nums[count], count+1, n);
+        dfs(nums,target,curSum-nums[count], count+1, n);
     }
     
     
     int findTargetSumWays(vector<int>& nums, int target) {
-
-        dfs(nums,target, 0, 0);
-        return ans;   
+        int n = nums.size();
+        dfs(nums,target,0,0,n);
+        return ans;
     }
 };
