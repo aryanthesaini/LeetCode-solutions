4d17e217fb1a277ea4b092aff84122510ce3bb56
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-07-12 23:36:32 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.08%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+    
+    int solve (int n , vector<int>&dp){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(n<0) return 0;
+        if(dp[n]) return dp[n];
+        
+        return dp[n]= solve(n-1, dp)+solve(n-2, dp);
+        
+        
+    }
+    
+    
+    
     int climbStairs(int n) {
-          int steps[50]={0};
-     steps[0]=1;
-     steps[1]=2;
+        vector<int>dp(n+1,0);
+        int ans = solve(n, dp);
         
+        return ans;     
         
-     for(int i=2;i<n;i++)
-     {
-         steps[i]=steps[i-2]+steps[i-1];
-     }
-    return steps[n-1];
-
     }
 };
