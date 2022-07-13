dc31bc470fcd65ab6832769957908a183327cb16
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-07-13 11:23:46 +0530

    Time: 4 ms (40.07%), Space: 6.4 MB (70.83%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,42 +1,13 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        
-        int n = s.length();
-        int m= t.length();
-        
-        int dp[n+1][m+1];
-        
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 or j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]= 1+dp[i-1][j-1];
-                }
-                
-                else{
-                    dp[i][j]= max(dp[i][j-1], dp[i-1][j]);
-                }
-            }
-        }
-        
-//          for(int i=0;i<=n;i++){
-//             for(int j=0;j<=m;j++){
-                
-//                    cout<<dp[i][j]<<" ";
-//             }
-//             cout<<endl;
-                
-//         }
-        if(dp[n][m]==n) return true;
-        else return false;
+          int j = 0; 
+    for (int i = 0; i < t.length() && j < s.length(); i++)
+        if (s[j] == t[i])
+            j++;
+ 
+    // If all characters of str1 were found in str2
+    return (j == s.length());
         
     }
 };
