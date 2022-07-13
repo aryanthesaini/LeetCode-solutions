3643e89731386768cf16a8337beca95563ec8df3
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-07-13 12:22:34 +0530

    Time: 11 ms (50.73%), Space: 7.2 MB (19.33%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        
         int n = s.length();
         int m = t.length();
         if(n!=m) return false;
+        
         
         map<char,char>mp;
         set<char>st;
@@ -12,13 +12,14 @@ public:
         for(int i=0;i<n;i++){
             
             if(mp.find(s[i])==mp.end() and st.find(t[i])==st.end()){
-                mp[s[i]]=t[i];
                 st.insert(t[i]);
+                mp[s[i]]=t[i];
             }
             
-            else if(st.find(t[i])!=st.end() and mp.find(s[i])==mp.end() ){
+            else if (st.find(t[i])!=st.end() and mp.find(s[i])==mp.end()){
                 return false;
             }
+            
             else if(mp[s[i]]!=t[i]){
                 return false;
             }
@@ -27,7 +28,6 @@ public:
         }
         
         return true;
-        
         
         
     }
