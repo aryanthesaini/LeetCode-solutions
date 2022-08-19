66999b1cb1dc7416c00e34767464321dc99e3fc9
Aryan Saini <68115113+aryanthesaini@users.noreply.github.com>
2022-08-19 13:23:31 +0530

    Time: 185 ms (93.04%), Space: 85 MB (86.80%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& t) {
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+         
+        int n = temperatures.size();
+        vector<int>ans(n, 0); 
+        stack<int>st{};
         
-        stack<pair<int,int>>st;
-        int n = t.size();
-        vector<pair<int,int>>ans;
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                ans.push_back({0,0});
-            }
-            else if(st.top().first>t[i]){
-                ans.push_back({st.top().first, st.top().second});
-            }
+		
+        for(int i = n-1; i>=0; --i){
             
-            else{
-                while(!st.empty() and st.top().first<=t[i]){
-                    st.pop();
-                }
-                if(st.empty()) ans.push_back({0,0});
-                else(ans.push_back({st.top().first, st.top().second}));
-            }
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i])
+                st.pop();		
             
-            st.push({t[i],i});
+            if(!st.empty())ans[i] = st.top()-i; 
+            st.push(i);
         }
-        vector<int>res;
-        reverse(ans.begin(), ans.end());
-        for(int i=0;i<n;i++){
-            if(ans[i].second==0) res.push_back(0);
-            else res.push_back(ans[i].second-i);
-        }
-        return res;
+        
+        return ans;
     }
 };
